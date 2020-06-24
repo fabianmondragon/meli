38da028e56798ce1b2df3d1dc03a3cdbe7aa12cc
fabianmondragon <mondra10@gmail.com>
2020-06-24 10:35:05 -0500

    add Constant

---
+++ app/src/main/java/com/example/myapplication/presentation/Constant.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.presentation
+
+class Constant {
+
+    companion object {
+        val ERROR_MSG = "Se produjo un error, por favor intente más tarde"
+        val NOT_FOUND = "No se obtuvo resultados"
+    }
+}
--- app/src/main/java/com/example/myapplication/presentation/searchproduct/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/presentation/searchproduct/MainViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.myapplication.presentation.searchproduct
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.myapplication.R
 
 import com.example.myapplication.UseCases
 
 import com.example.myapplication.di.DaggerMeliComponent
+import com.example.myapplication.presentation.Constant
 import com.example.myapplication.presentation.model.ResultPresentation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,6 +23,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     @Inject
     lateinit var useCases: UseCases
+    private val context: Context = application
 
     private val TAG = MainViewModel::class.simpleName
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
@@ -42,7 +46,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             coroutineScope.launch {
                 useCases.searchProduct.searchRepository(query!!)?.let {
                     if (it.size == 0) {
-                        messague.postValue("No se encontró resultados")
+                        messague.postValue(Constant.NOT_FOUND)
                     } else {
                         productList.postValue(
                             mapperDomainToPresentation.resultSearchDomainToPresentation(
@@ -54,20 +58,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             }
         } catch (exception: Exception) {
             Log.e(TAG, exception.message)
-            messague.value = "Ocurrio un error, por favor intente más tarde"
+            messague.value = Constant.ERROR_MSG
         } catch (nullExcpetion: NullPointerException) {
             Log.e(TAG, nullExcpetion.message)
-            messague.value = "Ocurrio un error, por favor intente más tarde"
+            messague.value = Constant.ERROR_MSG
         } catch (exception: IllegalArgumentException) {
             Log.e(TAG, exception.message)
-            messague.value = "Ocurrio un error, por favor intente más tarde"
+            messague.value = Constant.ERROR_MSG
         } catch (exception: IllegalStateException) {
             Log.e(TAG, exception.message)
-            messague.value = "Ocurrio un error, por favor intente más tarde"
+            messague.value = Constant.ERROR_MSG
 
         }
-
-
     }
 
     fun getOnInitialValueDetectedLiveData(): LiveData<List<ResultPresentation>> = productList
